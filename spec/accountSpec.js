54f2a8f30cc6e6364d438ca9f75ee39f9b0691f2
Harry Turnbull <hturnbull@live.co.uk>
2020-05-19 13:56:09 +0100

    R: withdraw reduces balance by 200 when passed 200

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -28,25 +28,34 @@ describe("Account", () => {
     it("returns str with balance: 10.50 when passed 10.50", () => {
       const account = new Account();
 
-      expect(account.deposit(10.50)).toEqual(
+      expect(account.deposit(10.5)).toEqual(
         "10.50 deposited. Current balance: 10.50"
-        );
-      });
+      );
     });
-    
-    describe('.withdraw', () => {
-      it("reduces balance by 100 when passed 100", () => {
-        const account = accountWith1000Deposited();
-        
-        expect(account.withdraw(100)).toEqual(
-          "100.00 withdrawn. Current balance: 900.00"
-      ); 
+  });
+
+  describe(".withdraw", () => {
+    it("reduces balance by 100 when passed 100", () => {
+      const account = accountWith1000Deposited();
+
+      expect(account.withdraw(100)).toEqual(
+        "100.00 withdrawn. Current balance: 900.00"
+      );
+    });
+
+    it("reduces balance by 200 when passed 200", () => {
+      const account = accountWith1000Deposited();
+
+      expect(account.withdraw(200)).toEqual(
+        "200.00 withdrawn. Current balance: 800.00"
+      );
     });
   });
 });
 
 function accountWith1000Deposited() {
-  let account = new Account()
-  account.deposit(1000)
-  return account
+  let account = new Account();
+  account.deposit(1000);
+  return account;
 }
+

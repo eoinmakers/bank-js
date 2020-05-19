9f88a10abff0d8ec4d7a24cd0b1e6499609b7d29
Harry Turnbull <hturnbull@live.co.uk>
2020-05-19 13:19:04 +0100

    R: returns str with deposited 100, balance: 300, when passed 200 then 100

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -2,15 +2,26 @@ describe("Account", () => {
   describe(".deposit", () => {
     it("returns str with balance: 100 when passed 100", () => {
       const account = new Account();
+
       expect(account.deposit(100)).toEqual(
         "100.00 deposited. Current balance: 100.00"
       );
     });
-    
-    it('returns str with balance: 200 when passed 200', () => {
+
+    it("returns str with balance: 200 when passed 200", () => {
       const account = new Account();
+
       expect(account.deposit(200)).toEqual(
         "200.00 deposited. Current balance: 200.00"
+      );
+    });
+
+    it("returns str with deposited 100, balance: 300, when passed 200 then 100", () => {
+      const account = new Account();
+      account.deposit(200);
+
+      expect(account.deposit(100)).toEqual(
+        "100.00 deposited. Current balance: 300.00"
       );
     });
   });

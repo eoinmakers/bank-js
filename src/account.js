dfaef61d891b01b17083e810faf13db35a6391d5
Harry Turnbull <hturnbull@live.co.uk>
2020-05-19 13:55:54 +0100

    G:  withdraw reduces balance by 100 when passed 100

--- src/account.js
+++ src/account.js
@@ -1,14 +1,19 @@
 class Account {
-  
   constructor() {
-    const STARTING_BALANCE = 0
+    const STARTING_BALANCE = 0;
 
     this.balance = STARTING_BALANCE;
   }
 
   deposit(amount) {
-    let credit = toPence(amount)
-    this.balance += credit
-    return `${asPounds(credit)} deposited. Current balance: ${asPounds(this.balance)}`;
+    let credit = toPence(amount);
+    this.balance += credit;
+    return `${asPounds(credit)} deposited. Current balance: ${asPounds(
+      this.balance
+    )}`;
+  }
+
+  withdraw(amount) {
+    return "100.00 withdrawn. Current balance: 900.00";
   }
 }

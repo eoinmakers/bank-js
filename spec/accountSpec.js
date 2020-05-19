61547bac580775bbc166941549d6e3131a45aefb
Harry Turnbull <hturnbull@live.co.uk>
2020-05-19 13:13:12 +0100

    clean up test descs

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,13 +1,13 @@
 describe("Account", () => {
   describe(".deposit", () => {
-    it("results in balance of 100 when passed 100", () => {
+    it("returns str with balance: 100 when passed 100", () => {
       const account = new Account();
       expect(account.deposit(100)).toEqual(
         "100.00 deposited. Current balance: 100.00"
       );
     });
     
-    it('results in balance of 200 when passed 200', () => {
+    it('returns str with balance: 200 when passed 200', () => {
       const account = new Account();
       expect(account.deposit(200)).toEqual(
         "200.00 deposited. Current balance: 200.00"
